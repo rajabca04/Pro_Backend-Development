4d1905bc0f3baf8218823db979106566f8d7f3f0
rajabca04 <rajabca201804@gmail.com>
2023-07-30 11:48:42 +0530

    First commit

---
+++ index.js
@@ -0,0 +1,60 @@
+const express = require('express')
+const dateFormat = require('date-format')
+const app = express()
+const PORT = 4000 || process.env.PORT
+
+app.get('/', (req, res)=>{
+    res.status(200).send('Hello World')
+})
+
+// Gate Route for /api/v1/instagram
+
+app.get('/api/v1/instagram',(req,res)=>{
+    const instagram = {
+        userName: 'ethical_raja_offical',
+        followers:599,
+        follows:59,
+        date:dateFormat.asString('dd.MM.yyyy - hh:mm:ss', new Date())
+    }
+     res.status(200).json(instagram)
+})
+
+// Gate Route for /api/v1/facebook
+
+app.get('/api/v1/facebook',(req,res)=>{
+    const facebook = {
+        userName: 'Raja kumar sharma',
+        followers:569,
+        follows:99,
+        date:dateFormat.asString('dd.MM.yyyy - hh:mm:ss', new Date())
+    }
+     res.status(200).json(facebook)
+})
+
+// Gate Route for /api/v1/linkedin
+
+app.get('/api/v1/linkedin',(req,res)=>{
+    const linkedin = {
+        userName: 'Raja Kumar',
+        followers:899,
+        follows:87,
+        date:dateFormat.asString('dd.MM.yyyy - hh:mm:ss', new Date())
+    }
+     res.status(200).json(linkedin)
+})
+
+// Gate Route for /api/v1/:token: it return params of url
+
+app.get('/api/v1/:token', (req, res)=>{
+    console.log(req.params.token);
+    res.status(200).json({param: req.params.token})
+})
+
+
+// app.get('/test', (req, res)=>{
+//     res.status(400).sendFile(__dirname + '/page.html')
+// })
+
+app.listen(PORT, ()=>{
+    console.log(`Server is running on port ${PORT}`);
+})
